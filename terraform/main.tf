f13c5590fd49d39d616690ae1270ae4f31843f85
Olivér Viktor Halász <viktoroliver.halasz@tesco.com>
2026-01-22 17:05:06 +0100

    feat: terraform secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,5 +2,21 @@
 resource "kubernetes_namespace" "photo-server" {
   metadata {
     name = "photo-server"
+    labels = "photo-server"
+  }
+}
+
+resource "kubernetes_secret" "photo-server-secrets" {
+  metadata {
+    name      = "secrets"
+    namespace = kubernetes_namespace.photo-server
+    labels = "photo-server"
+  }
+
+  data = {
+    postgres_username = var.postgres_user
+    postgres_password = var.postgres_password
+    redis_password = var.redis_password
+
   }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "postgres_user" {
+  type = string
+}
+
+variable "postgres_password" {
+  type = string
+}
+
+variable "redis_password" {
+  type = string
+}
